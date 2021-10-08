3611b3a79dbd5fac95244585b79c4d358a989704
Colby Garland <colbygarland@gmail.com>
2021-10-08 12:55:02 -0700

    More updates

--- components/Award.tsx
+++ components/Award.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import Confetti from 'react-confetti';
 import { Container } from '../components/Container';
 import { Content } from '../components/Content';
 import { Heading } from '../components/Heading';
@@ -13,15 +15,31 @@ export interface AwardProps {
 
 export const Award = (props: AwardProps) => {
   const { title, subtitle, image, person, style } = props;
+  const [visible, setVisible] = useState(false);
+
+  function handleOnClick() {
+    setVisible(true);
+  }
+
   return (
     <Content style={style}>
       <Container>
         <Heading>{title}</Heading>
         <SubHeading>{subtitle}</SubHeading>
         <SubHeading>
-          <span className="text-4xl">
-            Current Panagoer: <span className="font-sansBold text-secondary">{person}</span>
-          </span>
+          <span className="text-4xl">Current Panagoer:</span>
+          {visible ? (
+            <>
+              <span className="ml-10 inline-block font-sansBold text-secondary">{person}</span>
+            </>
+          ) : (
+            <button
+              onClick={handleOnClick}
+              className=" ml-10 px-10 py-2 bg-secondary border-secondary border-4 text-white font-sansBold rounded-full transition duration-100 hover:bg-transparent hover:text-secondary"
+            >
+              Reveal
+            </button>
+          )}
         </SubHeading>
         {image ? <img src={image} alt="Award" /> : ''}
       </Container>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
 export const Footer = () => {
   return (
-    <footer className="px-20 py-10 text-center bg-primary">
-      <p className="text-gray-700 font-sansBold text-xl mb-0">Made with 💖 by Colby Garland</p>
+    <footer className="text-cente">
+      <div className="bg-secondary h-10"></div>
+      <div className="bg-tertiary h-10"></div>
+      <div className="px-20 py-10 bg-primary">
+        <p className="text-gray-700 font-sansBold text-xl mb-0">Made with 💖 by Colby Garland</p>
+      </div>
     </footer>
   );
 };
